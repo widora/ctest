dcf25e5710f0489c7d02e81cefce50be539c31ed
Midas <midaszhou@yahoo.com>
2016-12-24 20:45:52 +0800

    2016-12-24

--- ads-b.c
+++ ads-b.c
@@ -1,16 +1,27 @@
+/*------------------------------------------------
+
+Try to decode ADS-B messages
+
+
+midaszhou@qq.com
+-------------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> //-strlen()
+#include <math.h> //-floor() pow() cos() acos()
 
 #define CODE_BIN_LENGTH 112 
 #define CODE_HEX_LENGTH 28
 char LOOKUP_TABLE[]="#ABCDEFGHIJKLMNOPQRSTUVWXYZ#####_###############0123456789######";
+#define EVEN_FRAME 0
+#define ODD_FRAME 1
+#define NZ 15  //--Number of geographic latitude zones between equator and a pole. NZ=15 for Mode-S CPR encoding.
 
-
-/*-------------function  strmid() ----------------------------
+/*------------------   function  strmid()   ------------------------
 to copy 'n' chars from 'src' starting from 'm'_th char to 'dst'
-------------------------------------------------------------*/
-char* strmid(char *dst,char *src,int n,int m)  
+------------------------------------------------------------------*/
+static char* strmid(char *dst,char *src,int n,int m)  
 {
  char *p=src;
  char *q=dst;
@@ -24,21 +35,40 @@ char* strmid(char *dst,char *src,int n,int m)
  return dst;
 }
 
+/*-----------------------    function cal_NL()   -------------------------
+calcuate number of longitude zones by given value of latitude angle 
+--------------------------------------------------------------------*/
+static int cal_NL(double lat)
+{
+ double pi=3.1415926535897;  //--double 15~16 digital precision
 
+ if(lat==0) return 59;
+ else if(lat==87) return 2;
+ else if(lat==-87) return 2;
+ else if(lat > 87) return 1;
+ else if(lat < -87) return 1;
+ else
+   return floor(2*pi/acos(1-(1-cos(pi/2.0/NZ))/pow(cos(pi/180.0*lat),2)));
+}
 
-void main(void)
+void main(int argc, char* argv[])
 {
 
-char* str_HEX_CODE="8D4840D6202CC371C32CE0576098";
+//char* str_HEX_CODE="8D4840D6202CC371C32CE0576098";
+char* str_HEX_CODE="8D40621D58C382D690C8AC2863A7";
+
+char str_ICAO24[6+1]=""; //--4*6=24bits
+int  int_ICAO24;
+int  int_FRAME; // 1 or 0
 char str_BIN_CODE[CODE_BIN_LENGTH-1];
 char str_Temp[8];
 char str_CALL_SIGN[8+1]="";
 int n_div=CODE_HEX_LENGTH/8+1; // =4
-int i,j,k;
+int i,j,k,tmp;
 int  int_CODE_DF; 
 int  int_CODE_TC;
+int  int_NL; //number of longitude zones
 
-//uint64_t ltmp; //temp. unsigned long type
 unsigned long bin32_code[4]; //store binary ADS-B CODE in 4 groups of 32bit array
 char str_32BIT[8*4];  
 
@@ -49,32 +79,54 @@ for(i=0;i<n_div;i++)
     bin32_code[i]=strtoul(str_Temp,NULL,16);
     //ltmp=strtoul(str_Temp,NULL,16);
     //ltoa(ltmp,str_32BIT,2);
-    printf("%x ",bin32_code[i]); 
+    printf("%x",bin32_code[i]); 
     //printf("%s \n",str_32BIT);
-       
  }
 
- 
+//-------------------------- get DF and TC ---------------------------- 
 int_CODE_DF=(bin32_code[0]>>(32-5))&(0b11111);
 int_CODE_TC=(bin32_code[1])>>(32-5)&(0b11111);
 
-//------ get call-sign -------------
-int tmp;
+//-------------------------- get int_ICAO24 ----------------------------
+int_ICAO24=(bin32_code[0]&0xffffff);
 
-for(j=0;j<4;j++) //--first 4 chars
- {
-   tmp=(bin32_code[1]>>(32-(14+6*j)))&(0x3f);
-   str_CALL_SIGN[j]=LOOKUP_TABLE[tmp];  
- }
-for(j=0;j<4;j++) //--after 4 chars
- {
-   tmp=(bin32_code[2]>>(32-(6+6*j)))&(0x3f);
-   str_CALL_SIGN[j+4]=LOOKUP_TABLE[tmp];  
- }
+//-------------------------  get int_FRAME  ----------------------------
+int_FRAME=(bin32_code[1]>>(32-(54-32)))&(0b1);
 
 
+if(int_CODE_TC>0 && int_CODE_TC<5)  //-------DF=17, TC=1to4  Aircraft identification
+{
+//-------------------------  get CALL-SIGN  ----------------------------
+  for(j=0;j<4;j++) //--first 4 chars
+  {
+    tmp=(bin32_code[1]>>(32-(14+6*j)))&(0x3f);
+    str_CALL_SIGN[j]=LOOKUP_TABLE[tmp];  
+  }
+  for(j=0;j<4;j++) //--after 4 chars
+  {
+    tmp=(bin32_code[2]>>(32-(6+6*j)))&(0x3f);
+    str_CALL_SIGN[j+4]=LOOKUP_TABLE[tmp];  
+  }
+}
+
+if((int_CODE_TC>8 && int_CODE_TC<19) || (int_CODE_TC>19 && int_CODE_TC<23))//-----Airborne position
+//---TC=9to18 Airborne position (Baro Alt)  TC=20to22 Airborne position (GNSS Height)
+{
+ double lat=atof(argv[1]);  
+
+ int_NL=cal_NL(lat);
+ printf("int_NL=%d \n",int_NL);
+
+
+}
+
+
+
+printf("\n");
 printf("DF=%d \n",int_CODE_DF);
 printf("TC=%d \n",int_CODE_TC);
+printf("ICAO24=%x \n",int_ICAO24);
+printf("FRAME: %s \n",(int_FRAME>0)?"Odd":"Even");
 printf("CALL SIGN: %s \n",str_CALL_SIGN);
 
 
