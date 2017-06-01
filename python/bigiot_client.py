4857d9de0879b5427b023fe0473e98754421504a
Midas <midaszhou@yahoo.com>
2017-06-01 08:38:49 +0800

    bigiot_client.py: add exception handling

--- python/bigiot_client.py
+++ python/bigiot_client.py
@@ -17,6 +17,7 @@ import json
 import time
 from datetime import datetime
 import chitchat as cc
+import os 
 import sys
 
 #----- set char. encoding -------
@@ -27,9 +28,9 @@ sys.setdefaultencoding("utf-8")
 host = '121.42.180.30'
 port = 8181
 
-#设备ID及key
-DEVICEID='421'
-APIKEY='f80ea043e'
+#设备ID及key 
+DEVICEID='XXX'
+APIKEY='XXXXXXXXX'
 
 data = b''
 
@@ -61,6 +62,11 @@ json_checkout = json.dumps(checkout)
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.settimeout(0)  #--!!!!!!!! This will let s.recv(1) raise exception when receive nothing !!!!!
 
+def LogErr(error):
+        os.environ['error']=str(error) 
+	os.system("date >> /home/client_err.txt")
+	os.system("echo $error >> /home/client_err.txt")
+
 def ConnectSocket(host,port):
 	global s
 
@@ -70,7 +76,8 @@ def ConnectSocket(host,port):
 			s.connect((host,port)) #--one argument only!
 			break
 		except Exception, error:
-			#print error
+			LogErr(error)
+			LogErr("Err: ConnectSocket(host,port)")
 			#print('s.connect(host,port) fails,wait for 2 seconds!')
 			time.sleep(2)
 
@@ -84,6 +91,8 @@ def Checkin(json_checkin):
 
 	except Exception,error:
 		print error
+		LogErr(error)
+		LogErr('ERR: Checkin(json_checkin)')
 
 def Checkout(json_checkout):
 	try:
@@ -92,14 +101,17 @@ def Checkout(json_checkout):
 		print "Send msg=%s finish" %(json_checkout)
 	except Exception,error:
 		print error
+		LogErr(error)
+		LogErr("ERR: Checkout(json_checkout)")
 
 def keepOnline():
-	global KEEP_ONLINE_TIMEOUT
-	global t_start_keeponline,t_start_keepcheckin
-	global flag_during_keeponline
-	global count_try_keeponline
-	global flag_status_checkin
-	
+   global KEEP_ONLINE_TIMEOUT
+   global t_start_keeponline,t_start_keepcheckin
+   global flag_during_keeponline
+   global count_try_keeponline
+   global flag_status_checkin
+
+   try:
 	if flag_status_checkin == True and time.time()-t_start_keeponline >KEEP_ONLINE_TIMEOUT:
 		#----------- check whether last keeponline action is finished -----
 		if flag_during_keeponline == True:
@@ -129,6 +141,11 @@ def keepOnline():
 			print('send check stauts to sustain connection... sendall() fail!')
 		t_start_keeponline= time.time() #--renew t
 
+   except Exception,error:
+	  print error
+	  LogErr(error)
+	  LogErr("ERR: keepOnline()") 
+
 def keepCheckin():
 	global s
 	global CHECKIN_TIMEOUT
@@ -136,7 +153,8 @@ def keepCheckin():
 	global flag_status_checkin
 	
 	#print "-----enter keepChechin()"
-	if  flag_status_checkin == False and time.time()-t_start_keepcheckin > CHECKIN_TIMEOUT:
+	try:
+	   if  flag_status_checkin == False and time.time()-t_start_keepcheckin > CHECKIN_TIMEOUT:
                 print "------------Start s.close()"
                 s.close()
                 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -146,20 +164,43 @@ def keepCheckin():
                 print "------------Start checking in to BIGIOT..."
                 Checkin(json_checkin) 
                 t_start_keepcheckin=time.time()
-		
-	
+	except Exception,error:
+	   print error
+	   LogErr(error)
+	   LogErr("ERR: keepCheckin()")
+
 def say(s, id, coutent):
-	saydata = {"M":"say", "ID":id, "C":coutent }
-	json_say = json.dumps(saydata)
-	s.send(json_say.encode('utf-8'))
-	s.send(b'\n')
+	try:
+		saydata = {"M":"say", "ID":id, "C":coutent }
+		json_say = json.dumps(saydata)
+		s.send(json_say.encode('utf-8'))
+		s.send(b'\n')
+
+	except Exception,error:
+		print error
+		LogErr(error)
+		LogErr("ERR: say(s, id, coutent)")
+
+def getvoice(words):
+	try:
+		words=words.replace(" ","") #--remove all blanks
+		print "收到的回复:  ",words.encode()
+		strCMD='/home/getvoice '+'"'+words+'啊啊'+'"'
+		print strCMD.encode()
+		os.system(strCMD)
+
+	except Exeption,error:
+		print error
+		LogErr(error)
+		LogErr("ERR: getvoice(words)")
 
 def process(msg, s, json_checkin):
-	global t
-	global flag_during_keeponline
-	global count_try_keeponline	
-	global flag_status_checkin
+   global t
+   global flag_during_keeponline
+   global count_try_keeponline	
+   global flag_status_checkin
 
+   try:
 	#print "Begin json.loads(msg)"
 	json_data = json.loads(msg)
 	#print(json_data)
@@ -173,14 +214,15 @@ def process(msg, s, json_checkin):
 		print("Checkout succeed! msg=",json_data)
 	if json_data['M'] == 'say':
 		print("--------------------------------")
-
 		print "接收到的数据：", json_data
 		print "平台指令：:  ",json_data['C'].encode()
-		#say(s, json_data['ID'], cc.chitchat(json_data['C']))
+		#-------- speak input words
+		getvoice(json_data['C'].encode()) 
 		chat=cc.chitchat(json_data['C'])
-		chat=chat.replace(" ","")
 		print "收到的回复:  ",chat.encode()
-		#say(s, json_data['ID'], cc.chitchat(json_data['C']))
+		#------- speak tuling replying words
+		getvoice(chat)	
+		#-------  rely to BIGIOT 	
 		say(s, json_data['ID'], chat)
 	if json_data['M'] == 'connected':
 		s.send(json_checkin.encode('utf-8'))
@@ -195,15 +237,24 @@ def process(msg, s, json_checkin):
 		#t=time.time()
 		print("--------- checkin to BIGIOT succeed! -------")
 
+   except Exception,error:
+	print error	 
+	LogErr(error)
+	LogErr("ERR: process(msg, s, json_checkin)")
+
 #--------------------------  MAIN FUNC  -------------------------------
 ConnectSocket(host,port)
 Checkin(json_checkin)		
 while True:
 	try:
 		#------ try to receive msg ------------
+                print
 		d=s.recv(1)
 		flag_data_received=True
-	except:
+	except Exception,error:
+		print error
+		#---- recv nothing will trigger an exectpion
+		#LogErr(error)
 		flag_data_received=False
 		time.sleep(2)
 		keepOnline() #--- try to sustain login if connected
@@ -219,3 +270,4 @@ while True:
                         process(msg,s,json_checkin)
 			print "Finish processing msg."
 			data=b''
+
