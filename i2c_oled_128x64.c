b3b9cdbbabda645441ba0fbf1e7147b44a5fab7e
Midas <midaszhou@yahoo.com>
2017-07-12 11:39:02 +0800

    rearrange msg queue functions

--- i2c_oled_128x64.c
+++ i2c_oled_128x64.c
@@ -4,6 +4,8 @@
 #include <sys/msg.h>
 #include "msg_common.h"
 
+
+
 //===================== MAIN ==================
 
 void main()
@@ -12,11 +14,11 @@ void main()
  char strtime[10]={0};
  char strRSSI[5]={0};
  char strOledBuf[16]={0}; //--for oled line show
- //----for message IPC ----
+
+ //--- for IPC message queque --
  int msg_id=-1;
- struct st_msg msg_data; //message data
- long int msg_type=0; //message type
- struct msqid_ds msg_conf;//msg configuration set
+ key_t msg_key=5678;
+ char* msg_text; 
 
  //----for time string ----
  time_t timep;
@@ -33,30 +35,13 @@ void main()
  clearOledV();
  usleep(300000);
 
- //----- create message IPC ------
-  msg_id=msgget((key_t)5678,0666 | IPC_CREAT);
-  if(msg_id<0)
-  {
-	perror("msgget error");
- 	exit(-1);
-   }
-  else
-	printf("msgget: IPC create successfully!\n");
+ //---- create message queue ------
+ if((msg_id=createMsgQue(msg_key))<0)
+ {
+	printf("create message queue fails!\n");
+	exit(-1);
+ }
 
-  //----!!!! Minimize msg queue size to accumulation of msg -------
-  if(msgctl(msg_id,IPC_STAT,&msg_conf)==0)
-  {
-	msg_conf.msg_qbytes=65;
-	if(msgctl(msg_id,IPC_SET,&msg_conf)==0) 
-		printf("msgctl: reset msg_conf successfully!\n");
-  }
-/*
- //--- delete obselete data in IPC if any
-  if(msgctl(msg_id,IPC_RMID,0)<0)
-	printf("msgctl IPC_RMID failed!\n");
-  else
-	printf("Old data in IPC deleted!\n");
-*/
 
   drawOledStr16x8(0,0,"  Widora-NEO! ");
 
@@ -69,31 +54,35 @@ void main()
 	printf("strtime=%s\n",strtime);
 
 	//---- oled show ----
-//	drawOledStr16x8(2,0,"    ");
+	drawOledStr16x8(2,0,"    ");
 	drawOledStr16x8(2,31,strtime);
-//	drawOledStr16x8(2,95,"    ");
+	drawOledStr16x8(2,95,"    ");
 
-	//-----get IPC message-----
-	if(msgrcv(msg_id,(void *)&msg_data,MSG_BUFSIZE,msg_type,0) < 0)
+	//------- receive mssage queue from Ting ------
+        msg_text=recvMsgQue(msg_id,0);
+	if(msg_text!=NULL)
 	{
-		printf("No IPC message received!\n");
-		//---put IPC msg to oled ---
-		drawOledStr16x8(4,0," Ting: No data! ");
+		//---get RSSI--
+		strncpy(strRSSI,g_msg_data.text+3,4);
+		//---put to oled ---
+		sprintf(strOledBuf," Ting: %d dbm ",atoi(strRSSI));
+		drawOledStr16x8(4,0,strOledBuf);
 	}
 	else
-	{
-		printf("IPC Message:%s\n",msg_data.text);
-		//---get RSSI--
-		strncpy(strRSSI,msg_data.text+3,4);
-		//---put IPC msg to oled ---
-		sprintf(strOledBuf," Ting: %d dbm ",atoi(strRSSI));
-		drawOledStr16x8(4,0,strOledBuf);//msg_data.text);
-	}
+		drawOledStr16x8(4,0," Ting: No data! ");
+
 
 	//----- sleep a while ----
 	usleep(300000); // to short time may blur oled --200k ~1000k same cpu load
   }
-  //---delete IPC message----
+
+ //--- delete message queue -----
+/*
+  if(msgctl(msg_id,IPC_RMID,0)<0)
+	printf("msgctl IPC_RMID failed!\n");
+  else
+	printf("Old data in IPC deleted!\n");
+*/
 
   //---unlock i2c fd ----
   intFcntlOp(g_fdoled, F_SETLK, F_UNLCK, 0, SEEK_SET,10);
--- msg_common.h
+++ msg_common.h
@@ -5,12 +5,74 @@ Common head for message IPV communication
 #define __MSG_COMMON_H__
 
 #define MSG_BUFSIZE  64
+#define MSG_TYPE_TING 1
+#define MSG_TYPE_CC1101 2
 
-struct st_msg
+struct g_st_msg
 {
 	long int msg_type;
 	char text[MSG_BUFSIZE];
 };
 
+
+// int msg_id=-1;
+static struct g_st_msg g_msg_data; //message data
+static long int msg_type=0; //message type
+// struct msqid_ds msg_conf;//msg configuration set
+
+
+/*-------------------------------------------------------
+ create message queue and return message queue identifier
+-------------------------------------------------------*/
+static int createMsgQue(key_t key)
+{
+  int msg_id=-1;
+  struct msqid_ds msg_conf;//msg configuration set
+
+  //----- create message IPC ------
+  msg_id=msgget(key,0666 | IPC_CREAT);
+  if(msg_id<0)
+  {
+        perror("msgget error");
+        return msg_id;
+   }
+
+  //----!!!! Minimize msg queue size to eliminate accumulation of msg -------
+  if(msgctl(msg_id,IPC_STAT,&msg_conf)==0)
+  {
+        msg_conf.msg_qbytes=65;
+        if(msgctl(msg_id,IPC_SET,&msg_conf)==0) 
+                printf("msgctl: reset msg_conf to allow only one message in the queue succeed!\n");
+  }
+  else
+        printf("msgctl: reset msg_conf to allow only one message in the queue fails!\n");
+
+  return msg_id;
+}
+
+
+/*-------------------------------------------------------
+ receive data in message queue with specified message type
+-------------------------------------------------------*/
+static char* recvMsgQue(int msg_id,int msg_type)
+{
+   int msg_ret=-1;
+
+   //?????? need to initialize msg_data ????? 
+   //-----get IPC message-----
+   msg_ret=msgrcv(msg_id,(void *)&g_msg_data,MSG_BUFSIZE,msg_type,0);  // return number of bytes copied into msg buf
+   if(msg_ret<0) //??? how about =0?
+   {
+        perror("msgrcv");
+        printf("No IPC message received!\n");
+        return NULL;
+   }
+   else
+   {
+        printf("IPC Message:%s\n",g_msg_data.text);
+        return g_msg_data.text;
+   }
+
+}
 
 #endif
