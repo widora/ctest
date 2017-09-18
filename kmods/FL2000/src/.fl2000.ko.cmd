8fe5ec196bb28a59e06b459d623de3ded0a15a7d
Midas <midaszhou@yahoo.com>
2017-09-18 19:29:30 +0800

    add FL2000

---
+++ kmods/FL2000/src/.fl2000.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/midas/ctest/kmods/FL2000/src/fl2000.ko := /home/midas/openwrt_widora/staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/bin/mipsel-openwrt-linux-ld -r  -m elf32ltsmip -T ./scripts/module-common.lds -s --build-id  -o /home/midas/ctest/kmods/FL2000/src/fl2000.ko /home/midas/ctest/kmods/FL2000/src/fl2000.o /home/midas/ctest/kmods/FL2000/src/fl2000.mod.o
