1dd26b8739024fda82bacd3c860edcad97231e29
Midas <midaszhou@yahoo.com>
2018-06-16 19:12:14 +0800

    adjust polling interval

--- usb/libusb_test.c
+++ usb/libusb_test.c
@@ -1,7 +1,12 @@
-/*----------------------------------------------------------------
+/*---------------------------------------------------------------------------
 Reference: https://blog.csdn.net/kangear/article/details/32176659
 
-----------------------------------------------------------------*/
+1. The Max. packet size for the interrupt endpoints:
+	64bytes for full-speed USB
+	1024bytes for high-speed USB
+2. For interrupt transfer, endpoint Min. polling interval is 1ms, better at 2ms.
+   bInterval value to polling interval: 2^(bInterval-1)ms
+-------------------------------------------------------------------------------*/
 
 #include <stdio.h>
 #include <sys/types.h>
@@ -44,11 +49,13 @@ int main(void)
    libusb_device_handle *dev_handle;
    libusb_context *ctx=NULL;
    struct libusb_transfer *trans;
-   unsigned char data[4]={0}; // wMaxPacketSize=4 bytes defined in USB 
+   unsigned char data[64]={0}; // full speed interrupt packet: max. 64bytes.
+   // wMaxPacketSize bytes defined in USB config. descriptor
    unsigned char data_in[4]={0};
    int r;
    ssize_t cnt;
    int i;
+   char strCMD[50]={0};
 
    r=libusb_init(&ctx);
    if(r<0)
@@ -82,6 +89,7 @@ int main(void)
    libusb_free_device_list(devs,1);
 
    //---- detach kernel driver if necessar ----
+   // this will remove hid driver ...
    if(libusb_kernel_driver_active(dev_handle,0)==1)
    {
 	if(libusb_detach_kernel_driver(dev_handle,0)==0)
@@ -102,8 +110,7 @@ int main(void)
 
 
    //---- allocate trans ----
-   trans=libusb_alloc_transfer(0);//transfer intended for non-isochronous endpoint should specify an iso_packet count of zero
-
+//   trans=libusb_alloc_transfer(0);//transfer intended for non-isochronous endpoint should specify an iso_packet count of zero
 
 /*-------------------------------------------------------------------------------
    int libusb_interrupt_transfer(struct libusb_device_handle * dev_handle,
@@ -124,33 +131,43 @@ Note:
        a complete lack of I/O. Because libusb may split your transfer into a number of chunks, and timeout error may results
        from not completing transferring all chunks.
     5. The default endpoint bInterval value is used as the polling interval.
-
+    6. Return 0 as success.
 --------------------------------------------------------------------------------*/
- data[0]=0x01;
+  data[64-1]=0x00;
+  i=0;
  //------------------ loop transfering data to EP1 ----------
  while(1)
  {
+    //---- shift data ---
+    data[64-1]+=1;
+
     //---- write to USB device EP 1 OUT ---
-    r=libusb_interrupt_transfer(dev_handle,0x01,data,1, &cnt, 200); //0x00:host->dev, 0x80:dev->host
-    if(!r)
+    r=libusb_interrupt_transfer(dev_handle,0x01,data, 64, &cnt, 200); //0x00:host->dev, 0x80:dev->host
+    if(r != 0)
 	perror("libusb_interrupt_transfer() EP1-Out error");
-    printf("%d bytes data have been transferred to EP01 .\n",cnt);
+    if(cnt != 64)
+	printf("!!!Only %d bytes data have been transferred to EP01 .\n",cnt);
+
     //---- read from EP 1 IN  ---
+/*
     r=libusb_interrupt_transfer(dev_handle,0x81,data_in,4, &cnt, 200); //0x00:host->dev, 0x80:dev->host
     if(!r)
 	perror("libusb_interrupt_transfer() EP1-IN error");
     printf("%d bytes frome EP1-IN,data_in[]= 0x%02x%02x%02x%02x \n",cnt,data_in[3],data_in[2],data_in[1],data_in[0]);
+*/
 
-    //---- shift data ---
-    data[0] = data[0]<<1;
-    if(data[0] == 0)
-	data[0]=0x01;
+    //---- espeak ----
+/*
+    sprintf(strCMD,"espeak --stdout -s 200 %d | aplay -N",data[0]);
+    printf("%s \n",strCMD);
+    system(strCMD);
+*/
 
-    usleep(900000);
+//    usleep(500000);
   }
 
    //---- free trans  ----
-   libusb_free_transfer(trans);
+//   libusb_free_transfer(trans);
 
    //----- close dev and exit ctx ----
    libusb_close(dev_handle);//close usb dev. 
