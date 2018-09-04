13e0da89c9fbd7e9ac32ba8d36fd5e3d9b22176a
midas-zhou <midaszhou@yahoo.com>
2018-09-04 16:34:08 +0800

    read UID

--- W25Q/w25q_test.c
+++ W25Q/w25q_test.c
@@ -17,9 +17,9 @@ W25Q128FV(QPI Mode)     17h		   6018h
 
 int main()
 {
-	uint8_t TxBuf[4]={0x90,0,0,0};
-	uint8_t RxBuf[4];
-
+	uint8_t TxBuf[16];
+	uint8_t RxBuf[16];
+	int i;
 
 	//----- init and open spi dev -----
         if( SPI_Open() != 0 )
@@ -28,6 +28,7 @@ int main()
 		printf("spi df opened successfuly!\n");
 
 	//----- send Instruction 0x90000000 to W25Q then get MID/DID
+	TxBuf[0]=0x90;
 	SPI_Write_then_Read(TxBuf,4,RxBuf,2);
 	printf("Manufacturer ID: 0x%02X,  Device ID: 0x%02X \n",RxBuf[0],RxBuf[1]);
 
@@ -35,6 +36,41 @@ int main()
 	TxBuf[0]=0x9F;
 	SPI_Write_then_Read(TxBuf,1,RxBuf,3);
 	printf("Manufacturer ID: 0x%02X, Memory Type: 0x%02X, Capacity: 0x%02X \n",RxBuf[0],RxBuf[1],RxBuf[2]);
+
+	//-----	Read Unique ID --4Bh
+	TxBuf[0]=0x4B;
+	SPI_Write_then_Read(TxBuf,5,RxBuf,8); //4-dummy bytes,then 64-bits UID
+	printf("64-bits Unique ID: 0x");
+	for(i=0;i<8;i++)
+		printf("%02X",RxBuf[i]);
+	printf("\n");
+
+
+
+	/*	Release Power-down/Device ID(ABh)	 */
+	TxBuf[0]=0xAB;
+	SPI_Write_then_Read(TxBuf,4,RxBuf,1); //followed by 3 dummy bytes, get 1-byte ID
+	printf("Release Power-down and get Device ID: 0x%02X\n",RxBuf[0]);
+
+	/*	Write Enable --06h	*/
+
+
+	//// Sector Erase 4k-bytes --20h, execute the Write Enable first.
+	//// 32K Block Erase 32k-bytes --52h, execute the Write Enable first.
+	//// 64K Block Erase 64k-bytes --D8h, execute the Write Enable first.
+	//// Chip Erase --C7h or 60h, execute the Write Enable first.
+
+	/*	Page Program --02h
+	The Page Program instruction allows from one byte to 256 bytes(a page) of data to be programmed at
+	previously erased (FFh) memory locations. A Write Enable instruction must be executed before the
+	device will accept the Page Program instruction.
+	*/
+
+	/*	Write Disable --04h	*/
+
+
+	//	Read Data  --03h
+
 
 
 	//---- close spi dev
