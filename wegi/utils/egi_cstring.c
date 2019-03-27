e92029a6d1292a2ac8575ec4227bb0878308c2f5
midas-zhou <midaszhou@yahoo.com>
2019-03-27 11:08:21 +0800

    test read_conf

--- wegi/utils/egi_cstring.c
+++ wegi/utils/egi_cstring.c
@@ -9,35 +9,36 @@ Midas Zhou
 #include <errno.h>
 #include "egi_cstring.h"
 
-
-/*-----------------------------------------------
-Trim spaces at back of a string, return a pointer
+/*--------------------------------------------------
+Trim all spaces at end of a string, return a pointer
 to the first non_space char.
 Return:
 	pointer	to a char	OK, spaces trimed.
         NULL			Input buf invalid
------------------------------------------------*/
+--------------------------------------------------*/
 char * cstr_trim_space(char *buf)
 {
 	char *ps, *pe;
 
 	if(buf==NULL)return NULL;
 
-	/* eat up front spaces */
+	/* skip front spaces */
 	ps=buf;
 	for(ps=buf; *ps==' '; ps++)
-	{};
+	{ };
 
-
-	/* eat up back spaces */
+	/* eat up back spaces/returns, replace with 0 */
 	for(  pe=buf+strlen(buf)-1;
-	      *pe==' ';
+	      *pe==' '|| *pe=='\n' || *pe=='\r' ;
 	      (*pe=0, pe-- ) )
 	{ };
 
-	/* if all spaces */
-	if(ps==pe) {
-		printf("%s: Input buff contains all but spaces.\n",__func__);
+	/* if all spaces, or end token
+	 * if ps==pe, means there is only ONE char in the line.
+	*/
+	if( (long)ps > (long)pe ) { //ps==pe || *ps=='\0' ) {
+//		printf("%s: Input line buff contains all but spaces. ps=%p, pe=%p \n",
+//					__func__,ps,pe);
 			return NULL;
 	}
 
@@ -45,7 +46,7 @@ char * cstr_trim_space(char *buf)
 	return ps;
 }
 
-/*---------------------------------------------------------------------
+/*----------------------------------------------------------------------------------
 Search given SECTION and KEY string in the config file, copy VALUE
 string to the char *value if found.
 
@@ -57,33 +58,46 @@ NOTE:
 1. A config file should be edited like this:
 # comment comment comment
 # comment comment commnet
-
-[ SECTION1]
+        # comment comment
+  [ SECTION1]
 KEY1 = VALUE1
 KEY2= VALUE2
 
+##########   comment
+	####comment
+##
 [SECTION2 ]
 KEY1=VALUE1
 KEY2= VALUE2
 ...
-2. All space will be ignored/trimmed in config file.
-3. Max. Line Length of config file to be 256-1. ( see. line_buff[256] )
-4. Max SECT/KEY/VALUE Length of config file to be 64-1. ( see. str_test[64] )
+
+1. Lines starting with '#' are deemed as comment lines.
+2. Lines starting wiht '[' are deemed as start/end/boundary of a SECTION.
+3. Non_comments lines containing a '=' are parsed as assignment for KEYs with VALUEs.
+4. All spaces beside SECTION/KEY/VALUE strings will be ignored/trimmed.
+5. If there are more than one section with the same name, only the first
+   one is valid, and others will be all neglected.
+
+		[[ ------  LIMITS -----  ]]
+6. Max. length for one line in a config file is 256-1. ( see. line_buff[256] )
+7. Max. length of a SECTION/KEY/VALUE string is 64-1. ( see. str_test[64] )
 
 Return:
-	>0	Not found
+	3	VALE string is NULL
+	2	Fail to find KEY string
+	1	Fail to find SECTION string
 	0	OK
 	<0	Fails
-----------------------------------------------------------------------*/
+------------------------------------------------------------------------------------*/
 int egi_get_config_value(char *sect, char *key, char* pvalue)
 {
-	int ret=1;
+	int lnum=0;
+	int ret=0;
 
 	FILE *fil;
-//	char *sect="IOT_CLIENT";
-//	char *key="server_ip";
 	char line_buff[256];
 	char *ps=NULL, *pe=NULL; /* start/end char pointer for [ and  ] */
+	char *pt=NULL;
 	char str_test[64];
 	int  found_sect=0; /* 0 - section not found, !0 - found */
 
@@ -99,23 +113,34 @@ int egi_get_config_value(char *sect, char *key, char* pvalue)
 		printf("Fail to open config file '%s', %s\n",EGI_CONFIG_PATH, strerror(errno));
 		return -2;
 	}
-	printf("Succeed to open '%s', with file descriptor %d. \n",EGI_CONFIG_PATH, fileno(fil));
+	//printf("Succeed to open '%s', with file descriptor %d. \n",EGI_CONFIG_PATH, fileno(fil));
 
 	fseek(fil,0,SEEK_SET);
 
 	/* Search SECTION and KEY line by line */
 	while(!feof(fil))
 	{
+		lnum++;
+
 		memset(line_buff,0,sizeof(line_buff));
 		fgets(line_buff,sizeof(line_buff),fil);
 		//printf("line_buff: %s\n",line_buff);
 
+		/* 0. cut the return key '\r' '\n' at end .*/
+		/*   OK, cstr_trim_space() will do it */
+
 		/* 1.  Search SECTION name in the line_buff */
 		if(!found_sect)
 		{
-			/* Ignore comment lines which start with '#' */
-			if( *cstr_trim_space(line_buff)=='#' ) {
-				printf("Comment: %s\n",line_buff);
+			/* Ignore comment lines starting with '#' */
+			ps=cstr_trim_space(line_buff);
+			/* get rid of all_space/empty line */
+			if(ps==NULL) {
+				printf("config file:: Line:%d is an empty line!\n", lnum);
+				continue;
+			}
+			else if( *ps=='#') {
+//				printf("Comment: %s\n",line_buff);
 				continue;
 			}
 			/* search SECTION name between '[' and ']'
@@ -130,33 +155,73 @@ int egi_get_config_value(char *sect, char *key, char* pvalue)
 
 				/* check if SECTION name matches */
 				if( strcmp(sect,cstr_trim_space(str_test))==0 ) {
-					printf("found SECTION:%s \n",str_test);
-
+					printf("Found SECTION:[%s] \n",str_test);
 					found_sect=1;
 				}
 			}
 		}
 		/* 2. Search KEY name in the line_buff */
-		else
+		else /* IF found_sect */
 		{
+			ps=cstr_trim_space(line_buff);
+			/* bypass blank line */
+			if( ps==NULL )continue;
+			/* bypass comment line */
+			else if( *ps=='#' ) {
+//				printf("Comment: %s\n",line_buff);
+				continue;
+			}
+			/* If first char is '[', it reaches the next SECTION, so give up. */
+			else if( *ps=='[' ) {
+				printf("Get bottom of the section, fail to find key '%s'.\n",key);
+				ret=2;
+				break;
+			}
+
 			/* find first '=' */
 			ps=strstr(line_buff,"=");
+			/* assure NOT null and '=' is NOT a starting char 
+			 * But, we can not exclude spaces before '='.
+			 */
 			if( ps!=NULL && ps != line_buff ) {
 				memset(str_test,0,sizeof(str_test));
-				strncpy(str_test, ps, ps-line_buff);
-				/* check if KEY name matches */
-				if( strcmp(key,cstr_trim_space(str_test))==0 ) {
+				/* get key name string */
+				strncpy(str_test, line_buff, ps-line_buff);
+//				printf(" key str_test: %s\n",str_test);
+				/* assure key name is not NULL */
+				if( (ps=cstr_trim_space(str_test))==NULL) {
+				   printf("Key name is NULL in line_buff: '%s' \n",line_buff);
+				   continue;
+				}
+				/* if key name matches */
+				else if( strcmp(key,ps)==0 ) {
 					//printf("found KEY:%s \n",str_test);
-					/* then pass VALUE to pvalue */
-					strcpy(pvalue, cstr_trim_space(ps+1));
-					printf("found KEY:%s, VAULE:%s \n", key,pvalue);
-					ret=0; /* OK, get it! */
-					break;
+					ps=strstr(line_buff,"="); /* !!! again, point ps to '=' */
+					pt=cstr_trim_space(ps+1);
+					/* Assure the value is NOT null */
+					if(pt==NULL) {
+					   printf("%s: Value of key [%s] is NULL in line_buff: '%s' \n",
+										__func__, key, line_buff);
+					   ret=3;
+					   break;
+					}
+					/* pass VALUE to pavlue */
+					else {
+					   strcpy(pvalue, pt);
+					   printf("%s: Found  Key:[%s],  Value:[%s] \n",
+										__func__, key,pvalue);
+					   ret=0; /* OK, get it! */
+					   break;
+					}
 				}
 			}
 		}
 
 	} /* end of while() */
+	if(!found_sect) {
+		printf("%s: Fail to find given SECTION:[%s] in config file.\n",__func__,sect);
+		ret=1;
+	}
 
 	fclose(fil);
 	return ret;
